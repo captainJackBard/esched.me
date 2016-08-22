6fdce72a3a17d0f0c8b906e003feb6f0546479d4
dj-diaz <denniel.joshua@gmail.com>
2016-08-22 20:32:42 +0800

    fix base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/esched.me/';
+$config['base_url'] = 'https://stormy-meadow-46152.herokuapp.com';
 
 /*
 |--------------------------------------------------------------------------
