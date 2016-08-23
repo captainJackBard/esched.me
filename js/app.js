f09ea49a399c4b3211a84a645a386271d6972101
dj-diaz <denniel.joshua@gmail.com>
2016-08-23 20:37:52 +0800

    remove auto signin

--- js/app.js
+++ js/app.js
@@ -4,7 +4,6 @@ app.config(function(BackandProvider) {
     BackandProvider.setAppName('dodongx');
     BackandProvider.setSignUpToken('a7a341b4-d4b3-46b1-a786-c7c4c1776719');
     BackandProvider.setAnonymousToken('047a5ffa-e1ee-457e-8141-a6f3ab54d8ca');
-    BackandProvider.runSigninAfterSignup(true);
 });
 
 app.run(function($rootScope, Backand) {
