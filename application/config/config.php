a4c66b2d45b62dece8b4a43bd1ac71a72ee52ad5
dj-diaz <denniel.joshua@gmail.com>
2016-08-24 13:42:20 +0800

    fix base url for updated domain

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://stormy-meadow-46152.herokuapp.com/';
+$config['base_url'] = 'http://esched.diaz.tech/';
 //$config['base_url'] = 'http://localhost/esched.me/';
 
 /*
